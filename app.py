abb7c360ffd165976d0e44155d15426349cbf0b0
JJ <jonjones320@gmail.com>
2024-04-30 12:53:40 -0700

    Updated Flask-LoginManager UserLoader

--- app.py
+++ app.py
@@ -32,8 +32,8 @@ login_manager.init_app(app)
 
 
 @login_manager.user_loader
-def load_user(user_id):
-    return User.get_id()
+def load_user(self):
+    return User
 
 
 #################################  Register/login/logout routes ############################################# 
